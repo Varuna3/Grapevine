39bc70d5a54e8c9d89a95d7f66f1aaac156524da
Payton Kentch <frankrlystan@gmail.com>
2023-09-05 15:13:00 -0600

    init app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import session from 'express-session'
+import morgan from 'morgan'
+import ViteExpress from 'vite-express'
+import Sequelize from 'sequelize'
+
+//middleware
+const app = express()
+
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.static('public'))
+app.use(
+  session({
+    secret: 'L3TM30UT-1MSTUCK1NY0URP0CK3T',
+    resave: false,
+    saveUninitialized: false,
+  })
+)
+
+ViteExpress.config({ printViteDevServerHost: true })
+
+//routes
+app.get('/api', (req, res) => {
+  res.send('Hello World!')
+})
+
+//open server
+ViteExpress.listen(app, 8000, () => {
+  console.log(`Hold ctrl and click this: http://localhost:8000/`)
+})
