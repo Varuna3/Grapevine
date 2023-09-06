eb2cbb0ff16d3383378f748173e9f330c311e106
Payton Kentch <frankrlystan@gmail.com>
2023-09-06 16:20:00 -0600

    endpoint for creating messages

--- server/controllers/createMessage.js
+++ server/controllers/createMessage.js
@@ -1,12 +1,27 @@
 import axios from 'axios'
-import { User, Invite, Server, Message, ServerUser } from '../database/seed.js'
+import { User, Server, Message } from '../database/seed.js'
 
+// PUT /api/server {message: 'some message here', server: 'server-name'}
 const createMessage = async (req, res) => {
+  const { server, message } = req.body
   const user = await User.findOne({ where: { username: req.session.user } })
   const s = await Server.findOne({ where: { name: server } })
-  const { server, message } = req.body
-  const m = await Message.create({ message, serverId: s.id })
-  res.send({ Success: true })
+  if (user && s) {
+    try {
+      const m = await user.createMessage({
+        message: message,
+        serverId: Number(s.id),
+      })
+      res.send({ Success: true })
+    } catch {
+      res.send({ Error: 'Message could not be stored. Please try again.' })
+    }
+  } else {
+    res.send({
+      Error:
+        'You probably forgot to login, or the server does not exist. Please try again.',
+    })
+  }
 }
 
 export default createMessage
