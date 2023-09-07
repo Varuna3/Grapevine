464af341a19a3d992344897ca0f38a0ab672309c
Nick McArthur <mcarthurnick@gmail.com>
2023-09-07 12:30:26 -0600

    added LoginModal component, connected to HomePage. Login functionality working.

---
+++ src/components/LoginModal.jsx
@@ -0,0 +1,60 @@
+import { useState, useRef, useEffect } from "react";
+import axios from "axios";
+
+
+const LoginModal = ({ showModal, setShowModal }) => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const modalRef = useRef();
+
+    useEffect(() => {
+        if (!modalRef.current) return;
+     
+        if (showModal) {
+          modalRef.current.showModal();
+        } else {
+          modalRef.current.close();
+        }
+      }, [showModal]);
+
+    const submitHandler = e => {
+        e.preventDefault()
+
+        axios.post('/api/login', {username, password})
+        .then(res => {
+            console.log('response', res.data)
+            if(res.data.Success === true){
+                setShowModal(false)
+            } else {
+                console.log('Error in submithandler')
+            }
+        })
+    }
+ 
+  return (
+    <dialog ref={modalRef}>
+        <form onSubmit={submitHandler}>
+                <label htmlFor="username">Username:</label>
+                    <input 
+                        onChange={e => setUsername(e.target.value)} 
+                        type="text" 
+                        id="username" 
+                        name="username"
+                        value={username}
+                    />
+                <label htmlFor="password">Password:</label>
+                    <input
+                        onChange={e => setPassword(e.target.value)}  
+                        type="password" 
+                        id="password" 
+                        name="password"
+                        value={password}
+                    />
+                <button type="submit">Login</button>
+            </form>
+    </dialog>
+  );
+};
+
+export default LoginModal;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,34 @@
 // Home Page: Page for displaying and creating new server messages.
 
 import { useState, useEffect } from 'react'
-
+import axios from 'axios'
 import InputArea from '../components/InputArea'
 import Messages from '../components/Messages'
+import LoginModal from '../components/LoginModal'
 
 import '../styles/home-page.scss'
 
 export default function HomePage({ messages }) {
   const [username, setUsername] = useState('')
+  const [showModal, setShowModal] = useState(false);
+
+  console.log('username', username)
 
   useEffect(() => {
-    // *********** USE THIS LINE AFTER WE HAVE LOGIN PAGE ******************
-    // *  axios.get('/api/username').then(({ data }) => setUsername(data)) *
-    // *********************************************************************
-
-    setUsername('Varuna')
+    axios.get('/api/username')
+      .then(({ data }) => {
+        if(data.Success){
+          setUsername(data.Success)
+        } else if(data.Error) {
+          // Alert user there was an error
+          console.log(data.Error)
+        }
+      })
   }, [])
 
   return (
     <main className='home-page'>
+      <LoginModal showModal={showModal} setShowModal={setShowModal} setUsername={setUsername}/>
       <Messages messages={messages} />
       <InputArea
         callback={message => {
