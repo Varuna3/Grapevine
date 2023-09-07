88595b18294359af3819a47dde53b69c6fa21fb3
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-09-07 10:59:10 -0600

    Linked new components to app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 // App: Component containing the entire application.
 
-import { useEffect } from 'react'
-import Home from './Mock_Homepage/Home'
+import { useEffect } from "react";
+
+import HomePage from "./pages/HomePage";
 
 export default function App() {
-  const customShenanigansHandler = () => {
-    socket.emit('custom', 'custom text')
-  }
-  useEffect(() => {
-    socket.connect()
-    socket.on('custom', data => {
-      console.log(data)
-    })
-  }, [])
+	const customShenanigansHandler = () => {
+		socket.emit("custom", "custom text");
+	}
 
-  return (
-    <>
-      <div>
-        <button onClick={customShenanigansHandler}>
-          custom event shenanigans
-        </button>
-      </div>
-      <hr></hr>
-      <Home />
-    </>
-  )
+	useEffect(() => {
+		socket.connect();
+		socket.on("custom", data => {
+			console.log(data);
+		});
+	}, []);
+
+	return (
+		<div>
+			<button onClick={customShenanigansHandler}>
+				custom event shenanigans
+			</button>
+			<hr />
+			<HomePage />
+		</div>
+	);
 }
