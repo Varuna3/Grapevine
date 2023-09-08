04a4b4901d4537028f37a52b3d2af146d42f95d7
Payton Kentch <frankrlystan@gmail.com>
2023-09-08 11:51:51 -0600

    add formatter

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -7,22 +7,26 @@ import '../styles/messages.scss'
 
 export default function Messages({ messages, setMessages }) {
   const [messageDivs, setMessageDivs] = useState([])
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [selectedServer, setSelectedServer] = useState(1)
   let ids = 0
 
   // use hard-code server id 1, because we haven't implemented "servers" yet
   // **should only run once, because we only want to pull from server one time every time the page loads
-  let tmpMessages = []
   useEffect(() => {
     axios
       .get(
-        `/api/messages/1` /*TODO: make this dynamic when we implement servers*/
+        `/api/messages/${selectedServer}` /*TODO: change this state variable when user selects new server*/,
       )
       .then(({ data }) => {
         let tmp = [] // --> use this because messageDivs will NOT actually change until end of useEffect
         let tmpMessages = []
-        data.forEach(e => {
+        data.forEach((e) => {
           ids++
-          let message = { username: e.user.username, message: e.message }
+          let message = {
+            username: e.user.username,
+            message: e.message,
+          }
           tmpMessages = [...tmpMessages, message]
           tmp = [...tmp, createMessageDiv(message, ids)]
         })
@@ -34,7 +38,7 @@ export default function Messages({ messages, setMessages }) {
   // every time someone sends a message, our "messages" state variable should change, and this useEffect should run again.
   useEffect(() => {
     let tmpMessageDivs = []
-    messages.forEach(e => {
+    messages.forEach((e) => {
       ids++
       tmpMessageDivs.push(createMessageDiv(e, ids))
     })
@@ -45,20 +49,20 @@ export default function Messages({ messages, setMessages }) {
   // given an object formatted like this: {username, message} spit out a "message div" that we can display
   function createMessageDiv(e, id) {
     return (
-      <article key={id} className='messages-chat'>
-        <picture className='messages-pfp'>
+      <article key={id} className="messages-chat">
+        <picture className="messages-pfp">
           <img
             src={`https://placehold.co/128?text=${e.username[0]}`}
-            alt='Mary Grapevine'
+            alt="Mary Grapevine"
           />
         </picture>
-        <div className='messages-content'>
-          <h4 className='messages-username'>{e.username}</h4>
-          <span className='messages-text'>{e.message}</span>
+        <div className="messages-content">
+          <h4 className="messages-username">{e.username}</h4>
+          <span className="messages-text">{e.message}</span>
         </div>
       </article>
     )
   }
 
-  return <div className='messages'>{messageDivs}</div>
+  return <div className="messages">{messageDivs}</div>
 }
