52af256ec959d867930ea6793a4bb499f717579a
Payton Kentch <frankrlystan@gmail.com>
2023-09-08 12:00:11 -0600

    more vscode settings

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,24 @@ import { useEffect, useState } from 'react'
 import HomePage from './pages/HomePage'
 
 export default function App() {
-  const [messages, setMessages] = useState([])
+    const [messages, setMessages] = useState([])
 
-  useEffect(() => {
-    socket.connect()
-    socket.on('new message', data => handleNewMessage(data))
-    return () => socket.disconnect()
-  }, [messages])
+    useEffect(() => {
+        socket.connect()
+        socket.on('new message', (data) => handleNewMessage(data))
+        return () => socket.disconnect()
+    }, [messages])
 
-  function handleNewMessage(data) {
-    setMessages([
-      ...messages,
-      { username: data.username, message: data.message },
-    ])
-  }
+    function handleNewMessage(data) {
+        setMessages([
+            ...messages,
+            { username: data.username, message: data.message },
+        ])
+    }
 
-  return (
-    <div>
-      <HomePage messages={messages} setMessages={setMessages} />
-    </div>
-  )
+    return (
+        <div>
+            <HomePage messages={messages} setMessages={setMessages} />
+        </div>
+    )
 }
