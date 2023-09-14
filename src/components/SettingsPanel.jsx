1daec86fcb3e381bdaca4db94013a5c555607ffc
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-09-14 10:46:19 -0600

    Added base styles for settings panel component.

--- src/components/SettingsPanel.jsx
+++ src/components/SettingsPanel.jsx
@@ -1,99 +1,82 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import { toast } from 'react-toastify'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-import '../styles/settings-panel.scss'
+import "../styles/settings-panel.scss";
 
-import Logout from './Logout'
-import CreateInvite from './CreateInvite'
-import ShowInvites from './ShowInvites'
+import Logout from "./Logout";
+import CreateInvite from "./CreateInvite";
+import ShowInvites from "./ShowInvites";
 
 export default function SettingsPanel({
-    showSettings,
-    setShowModal,
-    setMessages,
-    setCurrentServer,
-    setServerList,
-    currentServer,
-    showInvitesModal,
-    setShowInvitesModal,
+	showSettings,
+	setShowModal,
+	setMessages,
+	setCurrentServer,
+	setServerList,
+	currentServer,
+	showInvitesModal,
+	setShowInvitesModal,
 }) {
-    const [marginLeft, setMarginLeft] = useState('100vw')
-    const [invites, setInvites] = useState([])
+	const [invites, setInvites] = useState([]);
 
-    useEffect(() => {
-        showSettings
-            ? () => {
-                  setMarginLeft('60vw')
-              }
-            : () => {
-                  setMarginLeft('100vw')
-              }
-    }, [showSettings])
-
-    return (
-        <>
-            <div
-                className="settings-panel"
-                style={
-                    showSettings
-                        ? { marginLeft: 'calc(100vw - 320px)' }
-                        : { marginLeft: '100vw' }
-                }
-            >
-                <h1>Server Settings</h1>
-                <CreateInvite name={currentServer.name} />
-                <button
-                    onClick={async () => {
-                        if (currentServer.id) {
-                            setShowInvitesModal(true)
-                            const { data } = await axios.get(
-                                `/api/invites/${currentServer.id}`
-                            )
-                            setInvites(data.Success)
-                        } else {
-                            toast.error('Please select a server.')
-                        }
-                    }}
-                >
-                    Show Invites
-                </button>
-                <ShowInvites
-                    showInvitesModal={showInvitesModal}
-                    setShowInvitesModal={setShowInvitesModal}
-                    serverId={currentServer.id}
-                    invites={invites}
-                    setInvites={setInvites}
-                />
-                <button
-                    style={{ color: 'red' }}
-                    onClick={async () => {
-                        if (currentServer.name) {
-                            const { data } = await axios.delete('/api/server', {
-                                data: { id: currentServer.id },
-                            })
-                            if (data.Success) {
-                                toast.success(
-                                    `Server '${currentServer.name}' deleted. Refresh to see it disappear.`
-                                )
-                            } else {
-                                toast.error('Something went wrong.')
-                            }
-                        } else {
-                            toast.error('Please select a server.')
-                        }
-                    }}
-                >
-                    Delete Server
-                </button>
-                <h1>User Settings</h1>
-                <Logout
-                    setShowModal={setShowModal}
-                    setMessages={setMessages}
-                    setCurrentServer={setCurrentServer}
-                    setServerList={setServerList}
-                />
-            </div>
-        </>
-    )
+	return (
+		<div
+			className="settings-panel"
+			data-open={showSettings}>
+			<div className="settings-panel-content">
+				<h1>Server Settings</h1>
+				<CreateInvite name={currentServer.name} />
+				<button
+					onClick={async () => {
+						if (currentServer.id) {
+							setShowInvitesModal(true);
+							const { data } = await axios.get(
+								`/api/invites/${currentServer.id}`,
+							);
+							setInvites(data.Success);
+						} else {
+							toast.error("Please select a server.");
+						}
+					}}>
+					Show Invites
+				</button>
+				<ShowInvites
+					showInvitesModal={showInvitesModal}
+					setShowInvitesModal={setShowInvitesModal}
+					serverId={currentServer.id}
+					invites={invites}
+					setInvites={setInvites}
+				/>
+				<button
+					style={{ color: "red" }}
+					onClick={async () => {
+						if (currentServer.name) {
+							const { data } = await axios.delete("/api/server", {
+								data: { id: currentServer.id },
+							});
+							if (data.Success) {
+								toast.success(
+									`Server '${currentServer.name}' deleted. Refresh to see it disappear.`,
+								);
+							} else {
+								toast.error("Something went wrong.");
+							}
+						} else {
+							toast.error("Please select a server.");
+						}
+					}}>
+					Delete Server
+				</button>
+				<h1>User Settings</h1>
+				<Logout
+					setShowModal={setShowModal}
+					setMessages={setMessages}
+					setCurrentServer={setCurrentServer}
+					setServerList={setServerList}
+				/>
+			</div>
+		</div>
+	);
 }
+
