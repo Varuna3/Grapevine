50823b1376d6d2b18312da6584ac5fbc75e5d981
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-09-14 10:43:30 -0600

    Removed container in App component for styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,39 @@
 // App: Component containing the entire application.
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import HomePage from './pages/HomePage'
+import HomePage from "./pages/HomePage";
 
-import './styles/app.scss'
+import "./styles/app.scss";
 
 export default function App() {
-    const [messages, setMessages] = useState([])
-    const [currentServer, setCurrentServer] = useState({})
+	const [messages, setMessages] = useState([]);
+	const [currentServer, setCurrentServer] = useState({});
 
-    useEffect(() => {
-        socket.connect()
-        socket.on('new message', (data) => {
-            if (data.server === currentServer.id) handleNewMessage(data)
-        })
-        return () => socket.disconnect()
-    }, [messages])
+	useEffect(() => {
+		socket.connect();
+		socket.on("new message", (data) => {
+			if (data.server === currentServer.id) handleNewMessage(data);
+		});
+		return () => socket.disconnect();
+	}, [messages]);
 
-    function handleNewMessage(data) {
-        setMessages([
-            ...messages,
-            { username: data.username, message: data.message },
-        ])
-    }
+	function handleNewMessage(data) {
+		setMessages([
+			...messages,
+			{ username: data.username, message: data.message },
+		]);
+	}
 
-    return (
-        <div>
-            <HomePage
-                messages={messages}
-                setMessages={setMessages}
-                currentServer={currentServer}
-                setCurrentServer={setCurrentServer}
-            />
-        </div>
-    )
+	return (
+		<>
+			<HomePage
+				messages={messages}
+				setMessages={setMessages}
+				currentServer={currentServer}
+				setCurrentServer={setCurrentServer}
+			/>
+		</>
+	);
 }
+
