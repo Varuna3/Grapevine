d1fd75de78193c1d940dd9ec321e8dde25e21f66
Payton Kentch <frankrlystan@gmail.com>
2023-09-19 10:49:24 -0600

    fix emoji styling

--- src/components/MessageDiv.jsx
+++ src/components/MessageDiv.jsx
@@ -4,10 +4,29 @@ import { marked } from 'marked'
 
 import Button from './Button'
 import axios from 'axios'
+// import { EMOJIS } from '../assets/Emojis.json'
+import { VALID_CHARS } from '../assets/ValidChars.json'
 
 export default function MessageDiv({ e, messageId, serverId }) {
     const [showDelete, setShowDelete] = useState(false)
     const [deleted, setDeleted] = useState(false)
+
+    // const emojis = new Set([...EMOJIS])
+    const validChars = new Set([...VALID_CHARS])
+
+    var shouldBeLarge = false
+
+    if (e.message.length < 12 && !e.message.includes('img')) {
+        const message = e.message.split('')
+        for (let i = 0; i < message.length; i++) {
+            if (validChars.has(message[i])) {
+                shouldBeLarge = false
+                break
+            } else {
+                shouldBeLarge = true
+            }
+        }
+    }
 
     return (
         <article
@@ -25,17 +44,18 @@ export default function MessageDiv({ e, messageId, serverId }) {
                     alt={e.username}
                     className="messages-pfp"
                 />
+                {/*--> object.username */}
                 <h4 className="messages-username">{e.username}</h4>
             </div>
             <div className="messages-content">
-                {/*--> object.username */}
+                {/*--> object.message */}
                 <span
                     className="messages-text"
+                    style={shouldBeLarge ? { fontSize: 40 } : { fontSize: 15 }}
                     dangerouslySetInnerHTML={{
                         __html: DOMPurify.sanitize(marked.parse(e.message)),
                     }}
                 />
-                {/*--> object.message */}
             </div>
             {!deleted ? (
                 <div
