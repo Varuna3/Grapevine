966108a5c08a9ae84bb4a0dac9185f7ac26d1d90
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-09-19 15:09:37 -0600

    Changed settings panel to use Modal component.

--- src/components/SettingsPanel.jsx
+++ src/components/SettingsPanel.jsx
@@ -9,10 +9,12 @@ import CreateInvite from './CreateInvite'
 import ShowInvites from './ShowInvites'
 import EditUser from './EditUser'
 import Button from './Button'
+import Modal from './Modal'
 
 export default function SettingsPanel(props) {
     const {
         showSettings,
+        setShowSettings,
         setShowModal,
         setMessages,
         setCurrentServer,
@@ -31,88 +33,85 @@ export default function SettingsPanel(props) {
     const [invites, setInvites] = useState([])
 
     return (
-        <>
-            <div className="settings-panel" data-open={showSettings}>
-                <h1>Server Settings</h1>
-                <CreateInvite name={currentServer.name} />
-                <button
-                    onClick={async () => {
-                        if (currentServer.id) {
-                            setShowInvitesModal(true)
-                            const { data } = await axios.get(
-                                `/api/invites/${currentServer.id}`
+        <Modal open={showSettings} setOpen={setShowSettings} title="Settings">
+            <CreateInvite name={currentServer.name} />
+            <button
+                onClick={async () => {
+                    if (currentServer.id) {
+                        setShowInvitesModal(true)
+                        const { data } = await axios.get(
+                            `/api/invites/${currentServer.id}`
+                        )
+                        setInvites(data.Success)
+                    } else {
+                        toast.error('Please select a server.')
+                    }
+                }}
+            >
+                Show Invites
+            </button>
+            <ShowInvites
+                showInvitesModal={showInvitesModal}
+                setShowInvitesModal={setShowInvitesModal}
+                serverId={currentServer.id}
+                invites={invites}
+                setInvites={setInvites}
+            />
+            <button
+                style={{ color: 'red' }}
+                onClick={async () => {
+                    if (currentServer.name) {
+                        const { data } = await axios.delete('/api/server', {
+                            data: { id: currentServer.id },
+                        })
+                        if (data.Success) {
+                            toast.success(
+                                `Server '${currentServer.name}' deleted. Refresh to see it disappear.`
                             )
-                            setInvites(data.Success)
                         } else {
-                            toast.error('Please select a server.')
+                            toast.error('Something went wrong.')
                         }
-                    }}
-                >
-                    Show Invites
-                </button>
-                <ShowInvites
-                    showInvitesModal={showInvitesModal}
-                    setShowInvitesModal={setShowInvitesModal}
-                    serverId={currentServer.id}
-                    invites={invites}
-                    setInvites={setInvites}
-                />
-                <button
-                    style={{ color: 'red' }}
-                    onClick={async () => {
-                        if (currentServer.name) {
-                            const { data } = await axios.delete('/api/server', {
-                                data: { id: currentServer.id },
-                            })
-                            if (data.Success) {
-                                toast.success(
-                                    `Server '${currentServer.name}' deleted. Refresh to see it disappear.`
-                                )
-                            } else {
-                                toast.error('Something went wrong.')
-                            }
-                        } else {
-                            toast.error('Please select a server.')
-                        }
-                    }}
-                >
-                    Delete Server
-                </button>
-                <h1>User Settings</h1>
-                {isEditing ? (
-                    <>
-                        <EditUser
-                            username={username}
-                            imageURL={imageURL}
-                            email={email}
-                            setUsername={setUsername}
-                            setProfileImage={setProfileImage}
-                            setProfileEmail={setProfileEmail}
-                            setIsEditing={setIsEditing}
-                        />
-                    </>
-                ) : (
-                    <>
-                        <img src={imageURL} />
-                        {username}
-                        <br />
-                        {email}
-                        <Button
-                            action={() => {
-                                setIsEditing(true)
-                            }}
-                        >
-                            Edit Profile
-                        </Button>
-                        <Logout
-                            setShowModal={setShowModal}
-                            setMessages={setMessages}
-                            setCurrentServer={setCurrentServer}
-                            setServerList={setServerList}
-                        />
-                    </>
-                )}
-            </div>
-        </>
+                    } else {
+                        toast.error('Please select a server.')
+                    }
+                }}
+            >
+                Delete Server
+            </button>
+            <h1>User Settings</h1>
+            {isEditing ? (
+                <>
+                    <EditUser
+                        username={username}
+                        imageURL={imageURL}
+                        email={email}
+                        setUsername={setUsername}
+                        setProfileImage={setProfileImage}
+                        setProfileEmail={setProfileEmail}
+                        setIsEditing={setIsEditing}
+                    />
+                </>
+            ) : (
+                <>
+                    <img src={imageURL} />
+                    {username}
+                    <br />
+                    {email}
+                    <Button
+                        action={() => {
+                            setIsEditing(true)
+                        }}
+                    >
+                        Edit Profile
+                    </Button>
+                    <Logout
+                        setShowModal={setShowModal}
+                        setMessages={setMessages}
+                        setCurrentServer={setCurrentServer}
+                        setServerList={setServerList}
+                    />
+                </>
+            )}
+        </Modal>
     )
 }
