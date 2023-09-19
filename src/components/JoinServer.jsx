5b6d3e8c0602a2ddd0d05c53325659cd77ecabad
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-09-19 15:00:42 -0600

    Styled Join Server modal using Modal component.

--- src/components/JoinServer.jsx
+++ src/components/JoinServer.jsx
@@ -4,6 +4,7 @@ import { toast, ToastContainer } from 'react-toastify'
 import TextField from './TextField'
 import Button from './Button'
 import '../styles/join-server.scss'
+import Modal from './Modal'
 
 export default function JoinServer({
     showJoinServerModal,
@@ -11,21 +12,13 @@ export default function JoinServer({
 }) {
     const [input, setInput] = useState('')
 
-    const modalRef = useRef()
-
-    useEffect(() => {
-        if (!modalRef.current) return
-
-        if (showJoinServerModal) {
-            modalRef.current.showModal()
-        } else {
-            modalRef.current.close()
-        }
-    })
-
     return (
-        <dialog ref={modalRef} className="join-server">
-            {showJoinServerModal ? (
+        <Modal
+            open={showJoinServerModal}
+            setOpen={setShowJoinServerModal}
+            title="Join Server"
+        >
+            {showJoinServerModal && (
                 <ToastContainer
                     position="top-center"
                     autoClose={2500}
@@ -38,48 +31,35 @@ export default function JoinServer({
                     pauseOnHover={false}
                     theme="dark"
                 />
-            ) : (
-                <></>
             )}
-            <div className="join-server-wrapper">
-                <form
-                    onSubmit={async (e) => {
-                        e.preventDefault()
-                        const { data } = await axios.post('/api/server/join', {
-                            inviteString: input,
-                        })
-                        if (data.Success) {
-                            toast.success(data.Success)
-                        } else {
-                            toast.error(data.Error)
-                        }
+            <form
+                className="join-server"
+                onSubmit={async (e) => {
+                    e.preventDefault()
+                    const { data } = await axios.post('/api/server/join', {
+                        inviteString: input,
+                    })
+                    if (data.Success) {
+                        toast.success(data.Success)
+                    } else {
+                        toast.error(data.Error)
+                    }
+                }}
+            >
+                <TextField
+                    id="input"
+                    type="text"
+                    label="Enter Invite Code:"
+                    placeholder="h41rygr4pe"
+                    value={input}
+                    action={(e) => {
+                        setInput(e.target.value)
                     }}
-                >
-                    <h2 className="join-server-heading">
-                        Invite:
-                    </h2>
-                    <TextField
-                        id="input"
-                        type="text" 
-                        label=""
-                        placeholder="h41rygr4pe"
-                        value={input}
-                        action={(e) => {
-                            setInput(e.target.value)
-                        }}
-                    />
-                    <Button>Join Server</Button>
-                </form>
-                <Button
-                variant="danger"
-                    action={() => {
-                        setShowJoinServerModal(false)
-                        setInput('')
-                    }}
-                >
-                    Close
+                />
+                <Button variant="success" type="submit">
+                    Join Server
                 </Button>
-            </div>
-        </dialog>
+            </form>
+        </Modal>
     )
 }
